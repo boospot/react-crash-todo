e353f177f25e3da31ae77a6c533ea466b05186d1
Rao Abid <rao@booskills.com>
2020-09-30 18:32:27 +0500

    pushing build

---
+++ build/precache-manifest.df16f28aadedb18d7f803707269a46b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf46612308953695c7285f7b6c72d04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f50c1f23221f8a3d168",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bdf55073f44487e85cb",
+    "url": "/static/js/2.32ff2edf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32ff2edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f50c1f23221f8a3d168",
+    "url": "/static/js/main.09078edf.chunk.js"
+  },
+  {
+    "revision": "a354e25eb4ef299ab5ee",
+    "url": "/static/js/runtime-main.a63bd82c.js"
+  }
+]);
